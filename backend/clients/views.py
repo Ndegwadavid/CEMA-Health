afe44d9ed000c8d72f3cb30f28d571a08d01965e
Ndegwadavid <davidndegwa6869@gmail.com>
2025-04-25 02:11:57 +0300

    manage enrollment data in the clients module

--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from .models import Client
-from .serializers import ClientSerializer
+from .models import Client, Enrollment
+from .serializers import ClientSerializer, EnrollmentSerializer
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer

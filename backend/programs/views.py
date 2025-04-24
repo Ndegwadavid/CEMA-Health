952055b1b18f3fac883d79432c2c983496b20919
Ndegwadavid <davidndegwa6869@gmail.com>
2025-04-25 00:35:36 +0300

    Implement ProgramViewSet for handling Program model API requests

--- backend/programs/views.py
+++ backend/programs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Program
+from .serializers import ProgramSerializer
 
-# Create your views here.
+class ProgramViewSet(viewsets.ModelViewSet):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer

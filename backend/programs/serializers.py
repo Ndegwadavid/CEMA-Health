f5840eb270c73794588bf7155ba0ed4a12997033
Ndegwadavid <davidndegwa6869@gmail.com>
2025-04-25 00:48:05 +0300

    Add short_code field to ProgramSerializer

--- backend/programs/serializers.py
+++ backend/programs/serializers.py
@@ -4,4 +4,4 @@ from .models import Program
 class ProgramSerializer(serializers.ModelSerializer):
     class Meta:
         model = Program
-        fields = ['id', 'name', 'description', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'short_code', 'description', 'created_at', 'updated_at']

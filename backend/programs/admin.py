67393715242281207f7bf5c1b73e2f09cad7d7de
Ndegwadavid <davidndegwa6869@gmail.com>
2025-04-25 00:32:25 +0300

    Register Program model in Django admin with display and search fields

--- backend/programs/admin.py
+++ backend/programs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Program
 
-# Register your models here.
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    search_fields = ('name',)
